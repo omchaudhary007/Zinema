e2e2cb113a52524a838e3923611f3aee0d82e45d
omchaudhary007 <omchaudhary0730@gmail.com>
2023-06-15 20:16:00 +0530

    minor fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaRegUserCircle } from "react-icons/fa";
+import { FiSettings } from "react-icons/fi";
 import { useSelector, useDispatch } from "react-redux";
 import { authLogout } from "../store/storageSlice";
 import { toast } from "react-toastify";
@@ -32,7 +33,7 @@ const Navbar = () => {
   return (
     <header className="flex shadow-md py-4 px-4 sm:px-10 bg-white min-h-[70px] tracking-wide z-50 fixed w-full">
       <div className="flex flex-wrap items-center justify-between gap-5 w-full">
-        {/* Logo - Always visible */}
+        {/* Logo */}
         <Link to="/" className="flex-shrink-0">
           <img
             src="https://www.pngall.com/wp-content/uploads/2018/06/Cinema-PNG-Image-HD.png"
@@ -41,7 +42,7 @@ const Navbar = () => {
           />
         </Link>
 
-        {/* Hamburger menu overlay and nav */}
+        {/* Navigation Menu */}
         <div
           id="collapseMenu"
           className={`${
@@ -59,7 +60,8 @@ const Navbar = () => {
             }`}
             onClick={() => setMenuOpen(false)}
           ></div>
-          {/* Close button (only on mobile) */}
+
+          {/* Close button (mobile) */}
           <button
             id="toggleClose"
             className={`lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white w-9 h-9 flex items-center justify-center border ${
@@ -73,129 +75,62 @@ const Navbar = () => {
               className="w-3.5 h-3.5 fill-black"
               viewBox="0 0 320.591 320.591"
             >
-              <path
-                d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
-                data-original="#000000"
-              ></path>
-              <path
-                d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
-                data-original="#000000"
-              ></path>
+              <path d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"></path>
+              <path d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"></path>
             </svg>
           </button>
 
+          {/* Navigation Links */}
           <ul
             className={`lg:flex items-center justify-center gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50 transition-transform duration-300 ${
-              menuOpen
-                ? "translate-x-0"
-                : "max-lg:-translate-x-full"
+              menuOpen ? "translate-x-0" : "max-lg:-translate-x-full"
             }`}
-            style={{
-              transitionProperty: "transform",
-            }}
             onClick={() => setMenuOpen(false)}
           >
             <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-              <Link
-                to="/"
-                className="hover:text-red-700 text-red-700 block font-medium text-[15px]"
-              >
+              <Link to="/" className="hover:text-red-700 text-red-700 block font-medium text-[15px]">
                 Home
               </Link>
             </li>
             <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-              <Link
-                to="movies"
-                className="hover:text-red-700 text-slate-900 block font-medium text-[15px]"
-              >
+              <Link to="movies" className="hover:text-red-700 text-slate-900 block font-medium text-[15px]">
                 Movies
               </Link>
             </li>
             <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-              <Link
-                to="coming-soon"
-                className="hover:text-red-700 text-slate-900 block font-medium text-[15px]"
-              >
+              <Link to="coming-soon" className="hover:text-red-700 text-slate-900 block font-medium text-[15px]">
                 Coming Soon
               </Link>
             </li>
             <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-              <Link
-                to="theaters"
-                className="hover:text-red-700 text-slate-900 block font-medium text-[15px]"
-              >
+              <Link to="theaters" className="hover:text-red-700 text-slate-900 block font-medium text-[15px]">
                 Theaters
               </Link>
             </li>
             <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-              <Link
-                to="about"
-                className="hover:text-red-700 text-slate-900 block font-medium text-[15px]"
-              >
+              <Link to="about" className="hover:text-red-700 text-slate-900 block font-medium text-[15px]">
                 About
               </Link>
             </li>
             <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-              <Link
-                to="contact"
-                className="hover:text-red-700 text-slate-900 block font-medium text-[15px]"
-              >
+              <Link to="contact" className="hover:text-red-700 text-slate-900 block font-medium text-[15px]">
                 Contact
               </Link>
             </li>
-            {userId && isAdmin && (
-              <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-                <Link
-                  to="/admin"
-                  className="hover:text-red-700 text-slate-900 block font-medium text-[15px]"
-                >
-                  Admin Panel
-                </Link>
-              </li>
-            )}
-            {userId ? (
-              <>
-                <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-                  <Link
-                    to="profile"
-                    className="hover:text-red-700 text-slate-900 block font-medium text-[15px]"
-                  >
-                    Profile
-                  </Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-                  <Link
-                    to="login"
-                    className="hover:text-red-700 text-slate-900 block font-medium text-[15px]"
-                  >
-                    Login
-                  </Link>
-                </li>
-                <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
-                  <Link
-                    to="signup"
-                    className="hover:text-red-700 text-slate-900 block font-medium text-[15px]"
-                  >
-                    Sign up
-                  </Link>
-                </li>
-              </>
-            )}
           </ul>
         </div>
 
+        {/* Auth Buttons */}
         <div className="flex items-center space-x-4">
           {userId ? (
             <div className="flex items-center space-x-4">
               {isAdmin && (
                 <Link
                   to="/admin"
-                  className="px-4 py-2 text-sm rounded-full font-medium tracking-wide text-white bg-indigo-600 hover:bg-indigo-700 transition-all"
+                  className="flex items-center gap-2 px-4 py-2 text-sm rounded-lg font-medium tracking-wide text-white bg-gradient-to-r from-indigo-600 to-indigo-700 hover:from-indigo-700 hover:to-indigo-800 transition-all duration-200 shadow-md hover:shadow-lg"
                 >
-                  Admin Panel
+                  <FiSettings className="w-4 h-4" />
+                  <span>Admin Panel</span>
                 </Link>
               )}
               <Link to="profile" className="text-2xl hover:text-red-700 transition-colors">
@@ -203,29 +138,29 @@ const Navbar = () => {
               </Link>
               <button
                 onClick={handleLogout}
-                className="px-4 py-2 text-sm rounded-full font-medium tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all"
+                className="px-4 py-2 text-sm rounded-lg font-medium tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all duration-200"
               >
                 Logout
               </button>
             </div>
           ) : (
-            <>
+            <div className="flex items-center space-x-4">
               <Link
                 to="login"
-                className="px-4 py-2 text-sm rounded-full font-medium tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all"
+                className="px-4 py-2 text-sm rounded-lg font-medium tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all duration-200"
               >
                 Login
               </Link>
               <Link
                 to="signup"
-                className="px-4 py-2 text-sm rounded-full tracking-wide border bg-red-600 text-white font-medium hover:bg-red-700 transition-all"
+                className="px-4 py-2 text-sm rounded-lg tracking-wide border bg-gradient-to-r from-red-600 to-red-700 text-white font-medium hover:from-red-700 hover:to-red-800 transition-all duration-200 shadow-md hover:shadow-lg"
               >
                 Sign up
               </Link>
-            </>
+            </div>
           )}
 
-          {/* Hamburger open button */}
+          {/* Hamburger Menu Button */}
           <button
             id="toggleOpen"
             className={`lg:hidden ${menuOpen ? "hidden" : ""}`}
